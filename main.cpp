c8ce9a6bc99a5d0d939a1784a690046b4529c23d
binoll <onionlsfa@gmail.com>
2023-03-02 00:48:59 +0300

    Fixed bugs

--- main.cpp
+++ main.cpp
@@ -3,6 +3,30 @@
 int main() {
     BinaryTree<int> tree(10);
 
+    tree.add(30);
+
+    tree.add(5);
+
+    tree.add(7);
+
+    tree.add(3);
+
+    tree.add(20);
+
+    tree.add(40);
+
+    tree.add(1);
+
+    tree.add(4);
+
+    tree.remove(5);
+
+    tree.add(35);
+
+    tree.add(50);
+
+    tree.remove(40);
+
     std::cout << tree.getSize();
 
     return 0;
